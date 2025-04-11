512d53a55152e0d2f8e06080620945b78c5454a9
Aziz Tazrout <aztazrout@gmail.com>
2025-04-10 22:39:19 -0400

    Initialize Project

---
+++ src/main/java/com/azed/luxstay_hotels/LuxstayHotelsApplication.java
@@ -0,0 +1,13 @@
+package com.azed.luxstay_hotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxstayHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxstayHotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azed/luxstay_hotels/LuxstayHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azed.luxstay_hotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxstayHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
